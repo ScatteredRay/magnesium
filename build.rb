7359c8613b4cf8306ef4aa61a0774aef54fa04ea
Nicholas 'Indy' Ray <arelius@gmail.com>
2011-04-05 11:25:39 -0700

    Added protocol buffer test.
    * Created .proto file.
    * Used generated class to initialize the build.

--- build.rb
+++ build.rb
@@ -2,87 +2,70 @@ require 'rubygems'
 require 'git'
 require 'mustache'
 
-def init_build_directory(build_directory, git_repo)
-  begin
-    Dir.mkdir(build_directory)
-  rescue Errno::EEXIST
-    # Directory exists!
+module Build
+  def self.init_build_directory(build_directory, git_repo)
+    begin
+      Dir.mkdir(build_directory)
+    rescue Errno::EEXIST
+      # Directory exists!
+    end
+
+    begin
+      Git.clone(git_repo, build_directory);
+    rescue Git::GitExecuteError
+      # Git clone error.
+    end
   end
 
-  begin
-    Git.clone(git_repo, build_directory);
-  rescue Git::GitExecuteError
-    # Git clone error.
+  def self.install_certificate(dev_cert)
+    # Take care of certs and provisioning profiles.
+    cert_pass = "1233456789" # Perhaps we should generate this randomlly?
+    #system("security import #{dev_cert} -k ~/Library/Keychains/login.keychain -P #{cert_pass} -T /usr/bin/codesign")
+    # *.xcodeproj/project.pbxproj has CODE_SIGN_IDENTITY make sure those match the cert. just installed.
   end
-end
 
-def install_certificate(dev_cert)
-  # Take care of certs and provisioning profiles.
-  cert_pass = "1233456789" # Perhaps we should generate this randomlly?
-  #system("security import #{dev_cert} -k ~/Library/Keychains/login.keychain -P #{cert_pass} -T /usr/bin/codesign")
-  # *.xcodeproj/project.pbxproj has CODE_SIGN_IDENTITY make sure those match the cert. just installed.
-end
+  def self.run_xcode_build(build_directory, proj_name, target, config_name, sdk_name)
+    # Do the build.
+    source_dir = Dir.pwd
+    Dir.chdir(build_directory);
+    ret = system("xcodebuild -project #{proj_name} -target #{target} -configuration #{config_name} -sdk #{sdk_name}")
 
-def run_xcode_build(build_directory, proj_name, target, config_name, sdk_name)
-  # Do the build.
-  source_dir = Dir.pwd
-  Dir.chdir(build_directory);
-  ret = system("xcodebuild -project #{proj_name} -target #{target} -configuration #{config_name} -sdk #{sdk_name}")
-
-  if(!ret)
-    # build error
-    # $?
+    if(!ret)
+      # build error
+      # $?
+    end
+    Dir.chdir(source_dir);
   end
-  Dir.chdir(source_dir);
-end
 
-def build_ipa(build_directory, config_name, target)
-  source_dir = Dir.pwd
-  build_path = build_directory + "/build/#{config_name}-iphoneos"
-  app_path = build_path + "/#{target}.app"
-  ipa_path = build_path + "/#{target}.ipa"
-  payload = build_path + "/payload/"
+  def self.build_ipa(build_directory, config_name, target)
+    source_dir = Dir.pwd
+    build_path = build_directory + "/build/#{config_name}-iphoneos"
+    app_path = build_path + "/#{target}.app"
+    ipa_path = build_path + "/#{target}.ipa"
+    payload = build_path + "/payload/"
 
-  # Fail on error here.
-  Dir.mkdir(payload)
-  FileUtils.cp_r(app_path, payload)
+    # Fail on error here.
+    Dir.mkdir(payload)
+    FileUtils.cp_r(app_path, payload)
 
-  Dir.chdir(build_path)
-  ret = system("zip -r #{target}.ipa payload")
-  # fail on ret
-  Dir.chdir(source_dir);
+    Dir.chdir(build_path)
+    ret = system("zip -r #{target}.ipa payload")
+    # fail on ret
+    Dir.chdir(source_dir);
 
-  return ipa_path;
-end
+    return ipa_path;
+  end
 
-def render_manifest(dest_url, target, bundle_id, version, title)
-  Mustache.template_file = "manifest.plist"
-  view = Mustache.new
-  view[:PackageUrl] = dest_url + "#{target}.ipa"
-  view[:DisplayImageUrl] = dest_url + "Icon.png"
-  view[:FullImageUrl] = dest_url + "Icon.png"
+  def self.render_manifest(dest_url, target, bundle_id, version, title)
+    Mustache.template_file = "manifest.plist"
+    view = Mustache.new
+    view[:PackageUrl] = dest_url + "#{target}.ipa"
+    view[:DisplayImageUrl] = dest_url + "Icon.png"
+    view[:FullImageUrl] = dest_url + "Icon.png"
 
-  view[:bundle_id] = bundle_id
-  view[:version] = version
-  view[:title] = title
-  return view.render()
-end
-
-BuildDirectory = "/Users/indy/dev/tmpbuilddir"
-GitRepo = "/Users/indy/dev/Idtor"
-ProjName = "idtor.xcodeproj"
-Target = "Idtor"
-ConfigName = "Release"
-SDKName = "iphoneos4.3"
-
-DestUrl = "http://arelius.com/Idtor/"
-
-init_build_directory(BuildDirectory, GitRepo);
-install_certificate("Certificate.p12") # *.cer works?
-run_xcode_build(BuildDirectory, ProjName, Target, ConfigName, SDKName)
-build_ipa(BuildDirectory, ConfigName, Target)
-Manifest = render_manifest(DestUrl, Target, "com.ibuild.Idtor", "1.0 (1.0)", "Idtor")
-
-File.open(BuildDirectory + "/manifest.plist", "w") do |f|
-  f.write(Manifest)
+    view[:bundle_id] = bundle_id
+    view[:version] = version
+    view[:title] = title
+    return view.render()
+  end
 end
---
+++ build_daemon.rb
@@ -0,0 +1,47 @@
+require 'build'
+require 'build.pb'
+
+def do_build(message)
+  build_directory = "/Users/indy/dev/tmpbuilddir"
+  dest_url = "http://arelius.com/Idtor/"
+
+  Build.init_build_directory(build_directory, message.GitRepo);
+  Build.install_certificate("Certificate.p12") # *.cer works?
+  Build.run_xcode_build(build_directory,
+                        message.Project,
+                        message.Target,
+                        message.ConfigName,
+                        message.SDK)
+  ipa_file = Build.build_ipa(build_directory, message.ConfigName, message.Target)
+  # We need to pull this info out of the project.
+  manifest = Build.render_manifest(dest_url, message.Target,
+                                   "com.ibuild.Idtor",
+                                   "1.0 (1.0)",
+                                   "Idtor")
+  ret = BuildSuccess.new
+  ret.Manifest = manifest
+  # This can't work for large IPA's we need some sort of streaming protocol.
+  File.open(ipa_file, "r") do |f|
+    ret.IPA = f.read()
+  end
+  return ret;
+end
+
+GitRepo = "/Users/indy/dev/Idtor"
+ProjName = "idtor.xcodeproj"
+Target = "Idtor"
+ConfigName = "Release"
+SDKName = "iphoneos4.3"
+
+build_message = BuildBegin.new
+build_message.GitRepo = GitRepo
+build_message.Project = ProjName
+build_message.Target = Target
+build_message.ConfigName = ConfigName
+build_message.SDK = SDKName
+#File.open("Certificate.p12", "r") do |f|
+#  build_message.Certificate = f.read()
+#end
+build_message.Certificate = ""
+
+do_build(build_message)
